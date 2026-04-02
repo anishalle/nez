59a87ae8758fb63a4d7a956e76f4c7a7a2d4838b
anishalle <anish.alle11@gmail.com>
2026-04-02 00:18:10 -0500

    feat: add ignore flag

--- src/main.py
+++ src/main.py
@@ -3,6 +3,6 @@
 import RocketSim as rs
 
 # Make an arena instance (this is where our simulation takes place, has its own btDynamicsWorld instance)
-arena = rs.Arena(rs.GameMode.SOCCAR)
+arena = rs.Arena(rs.GameMode.SOCCAR)  # pyright: ignore[reportCallIssue]
 
 x = arena.get_boost_pads()
